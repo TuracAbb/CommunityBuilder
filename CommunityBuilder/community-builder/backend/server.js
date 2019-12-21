d6cd71a780a930c09fe46abc26a6461083923227
turac <turacabb97@gmail.com>
2019-12-21 13:56:35 +0300

    nodemon server added to package json

--- CommunityBuilder/community-builder/backend/server.js
+++ CommunityBuilder/community-builder/backend/server.js
@@ -6,14 +6,13 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
-/*const uri = process.env.MONGODB_URI;
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-);*/
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your-app-name');
+);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
