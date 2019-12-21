2f7cb72d58bcae35d4cd2f4b0f13616f8873279b
turac <turacabb97@gmail.com>
2019-12-21 13:29:22 +0300

    DB configurations changed

--- CommunityBuilder/community-builder/backend/server.js
+++ CommunityBuilder/community-builder/backend/server.js
@@ -6,13 +6,14 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+/*const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-);
+);*/
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your-app-name');
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
